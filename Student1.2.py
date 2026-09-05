61b64e534ec0b0af5a10fd51d543b8c0bdb69396
as6781 <noneas6781@gmail.com>
2026-09-05 14:21:57 +0300

    Add files via upload

--- Student1.2.py
+++ Student1.2.py
@@ -1,12 +1,12 @@
 
 def Grade(Marks):
-    if Students['Marks'] >= '90':
+    if Marks >= 90:
         return "A"
-    elif Students['Marks'] >= '80':
+    elif Marks >= 80:
         return "B"
-    elif Students['Marks'] >= '70':
+    elif Marks >= 70:
         return "C"
-    elif Students['Marks'] >= '60':
+    elif Marks >= 60:
         return "D"
     else:
         return "Fail"
@@ -17,18 +17,22 @@ Students = {
     "Name":input('Enter your name: '),
     "Age":input('Enter your age: '),
     "Student_ID":input('Enter your student ID: '),
-    "Marks":input('Enter your marks: ')
+    "Marks":int(input('Enter your marks: '))
+    
 }
+
+
+
 
 print("The students name is " + Students['Name'])
 print("The students age is " + Students['Age'])
 print("The students ID is " + Students['Student_ID'])
-print("The students marks are " + Students['Marks'])
+print("The students marks are " + str(Students['Marks']))
 print("The students grade is " + Grade(Students['Marks']))
 
 
 
-final_marks = input('Enter students final marks:')
+final_marks = int(input('Enter students final marks:'))
 Students['Marks'] = final_marks
 
 
